7e382960a4ad24adafbc2ae7643f44b339539bc7
Ajla Arnautovic <ajla.arnautovic@socialexplorer.com>
2021-05-18 20:56:58 +0200

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '82884536b3698eb097a02de74e6875120247b632',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '82884536b3698eb097a02de74e6875120247b632',
+    ),
+  ),
+);
