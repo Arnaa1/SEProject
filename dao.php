08251188f5c10c6dcbaa66ab1c3bae2426f6d45c
kbektas <bekta.kenan@gmail.com>
2021-06-19 21:01:57 +0200

    added coding standard

--- dao.php
+++ dao.php
@@ -2,17 +2,14 @@
 
 require 'flight/Flight.php';
 
-if (getenv('HEROKU'))
-{
+if (getenv('HEROKU')) {
     $username = getenv('USERNAME');
     $password = getenv('PASSWORD');
     $dbname = getenv('DBNAME');
 
     $host = "sql11.freemysqlhosting.net";
 
-}
-else
-{
+} else {
     $username = "root";
     $password = "rootroot";
     $dbname = "shopping_cart";
@@ -20,20 +17,17 @@ else
     $host = "localhost";
 }
 
-Flight::register("db", "PDO", array("mysql:host=$host;dbname=$dbname", $username, $password), function ($db)
-{
+Flight::register("db", "PDO", array("mysql:host=$host;dbname=$dbname", $username, $password), function ($db) {
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 });
 
 // list all the products in the products table
-Flight::route('GET /list-products', function ()
-{
+Flight::route('GET /list-products', function () {
 
     $db = Flight::db();
     $g = $db->query("SELECT * FROM products");
 
-    while ($row = $g->fetch(PDO::FETCH_ASSOC))
-    {
+    while ($row = $g->fetch(PDO::FETCH_ASSOC)) {
         $ar[] = $row;
     }
 
@@ -41,8 +35,7 @@ Flight::route('GET /list-products', function ()
 });
 
 // add item to cart
-Flight::route("POST /order", function ()
-{
+Flight::route("POST /order", function () {
 
     $db = Flight::db();
     $request = Flight::request();
@@ -54,28 +47,24 @@ Flight::route("POST /order", function ()
     $d = $db->prepare("INSERT INTO cart(name,price,quantity) VALUES (?,?,?)");
     $ch = $d->execute([$name, $price, $quantity]);
 
-    if ($ch)
-    {
+    if ($ch) {
         echo "yes";
     }
 
 });
 
 //return number of items in user cart
-Flight::route("GET /my-cart", function ()
-{
+Flight::route("GET /my-cart", function () {
     $db = Flight::db();
 
     $d = $db->query("SELECT * FROM cart");
 
-    while ($r = $d->fetch())
-    {
+    while ($r = $d->fetch()) {
         $num[] = $r;
     }
 
     $n = "0";
-    if (!empty($num))
-    {
+    if (!empty($num)) {
         $n = count($num);
     }
     //get numbers of item
@@ -84,14 +73,12 @@ Flight::route("GET /my-cart", function ()
 });
 
 // list all the items in user's cart
-Flight::route('GET /my-cart-items', function ()
-{
+Flight::route('GET /my-cart-items', function () {
 
     $db = Flight::db();
     $g = $db->query("SELECT * FROM cart");
 
-    while ($row = $g->fetch(PDO::FETCH_ASSOC))
-    {
+    while ($row = $g->fetch(PDO::FETCH_ASSOC)) {
         $ar[] = $row;
     }
 
@@ -99,16 +86,14 @@ Flight::route('GET /my-cart-items', function ()
 });
 
 //delete Item from cart
-Flight::route("GET /delete-item/@id", function ($id)
-{
+Flight::route("GET /delete-item/@id", function ($id) {
     $db = Flight::db();
     $s = $db->prepare("DELETE FROM cart where id = ?");
     $s->execute([$id]);
 });
 
 // checkout
-Flight::route("POST /checkout", function ()
-{
+Flight::route("POST /checkout", function () {
 
     $db = Flight::db();
     $request = Flight::request();
@@ -127,21 +112,18 @@ Flight::route("POST /checkout", function ()
 });
 
 //get total price
-Flight::route("GET /total-price", function ()
-{
+Flight::route("GET /total-price", function () {
     $db = Flight::db();
     $s = $db->query("SELECT * from cart");
     $p = 0;
-    while ($d = $s->fetch(PDO::FETCH_ASSOC))
-    {
+    while ($d = $s->fetch(PDO::FETCH_ASSOC)) {
         $p += $d["price"] * $d["quantity"];
     }
     echo $p;
 });
 
 //checkout receipt
-Flight::route("GET /receipt", function ()
-{
+Flight::route("GET /receipt", function () {
     $db = Flight::db();
     $s = $db->query("SELECT *  from orders ORDER BY ID DESC LIMIT 1");
     $d = $s->fetch(PDO::FETCH_ASSOC);
@@ -149,20 +131,16 @@ Flight::route("GET /receipt", function ()
 });
 
 //admin login
-Flight::route("POST /admin-login", function ()
-{
+Flight::route("POST /admin-login", function () {
 
     $re = Flight::request();
 
     $uname = $re->data->username;
     $pass = $re->data->password;
 
-    if ($uname == "ajla" & $pass == "ajla123")
-    {
+    if ($uname == "ajla" & $pass == "ajla123") {
         $n = 1;
-    }
-    else
-    {
+    } else {
         $n = 0;
     }
 
@@ -170,32 +148,26 @@ Flight::route("POST /admin-login", function ()
 });
 
 //admin
-Flight::route("GET /admin", function ()
-{
+Flight::route("GET /admin", function () {
 
     $db = Flight::db();
     $q = $db->query("SELECT * FROM orders");
 
     $orders = [];
-    while ($row = $q->fetch(PDO::FETCH_ASSOC))
-    {
+    while ($row = $q->fetch(PDO::FETCH_ASSOC)) {
         $orders[] = $row;
     }
 
-    if (count($orders) == 0)
-    {
+    if (count($orders) == 0) {
         echo "no";
-    }
-    else
-    {
+    } else {
         echo json_encode($orders);
     }
 
 });
 
 //delete order by admin
-Flight::route("POST /delete-order/@id", function ($id)
-{
+Flight::route("POST /delete-order/@id", function ($id) {
 
     $db = Flight::db();
     $s = $db->prepare("DELETE FROM orders where id = ?");
