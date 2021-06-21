9ed308e5e072b9a3e3b25a7432f615bab73e969e
kbektas <bekta.kenan@gmail.com>
2021-06-21 20:47:26 +0200

    minor changes

--- ConnectDb.php
+++ ConnectDb.php
@@ -19,7 +19,7 @@ class ConnectDb
             $this->pass = getenv('PASSWORD');
             $this->name = getenv('DBNAME');
 
-            $this->host = "localhost";
+            $this->host = "sql11.freemysqlhosting.net";
 
         } else {
             $this->user = "root";
@@ -30,7 +30,7 @@ class ConnectDb
         }
 
         $this->conn = new PDO("mysql:host={$this->host};
-    dbname={$this->name}", $this->user, $this->pass,
+        dbname={$this->name}", $this->user, $this->pass,
             array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
