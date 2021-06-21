d81957e1b8ced1ff16ab3b21b781dc0af8c24e52
kbektas <bekta.kenan@gmail.com>
2021-06-21 17:57:20 +0200

    builder/singleton pattern used

--- ConnectDb.php
+++ ConnectDb.php
@@ -1,14 +1,30 @@
 <?php
+
+require 'flight/Flight.php';
+
 class ConnectDb
 {
     // Hold the class instance.
     private static $instance = null;
     private $conn;
 
+    if (getenv('HEROKU')) {
+    $username = getenv('USERNAME');
+    $password = getenv('PASSWORD');
+    $dbname = getenv('DBNAME');
+
+    $host = "localhost";
+    } else { 
+
+
     private $host = 'localhost';
     private $user = 'root';
     private $pass = 'rootroot';
     private $name = 'shopping_cart';
+    }
+    Flight::register("db", "PDO", array("mysql:host=$host;dbname=$dbname", $username, $password), function ($db) {
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+});
 
     // The db connection is established in the private constructor.
     private function __construct()
